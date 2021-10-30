fc60b72d15689ee846327d706020a40e822ac2a6
Jf-Chen <485133380@qq.com>
2021-10-30 20:04:58 +0800

    update

--- FRN-rotation/trainers/eval.py
+++ FRN-rotation/trainers/eval.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 sys.path.append('..')
 from datasets import dataloaders
 from tqdm import tqdm
-import pdb
+
 
 def get_score(acc_list):
 
@@ -29,29 +29,11 @@ def meta_test(data_path,model,way,shot,pre,transform_type,query_shot=16,trial=10
     target = torch.LongTensor([i//query_shot for i in range(query_shot*way)]).cuda()
 
     acc_list = []
-    list_dist=torch.empty(0,way)
-    list_max_index=[]
-    # target始终不变
 
     for i, (inp,_) in tqdm(enumerate(eval_loader),position=0, leave=True):
 
         inp = inp.cuda()
         max_index = model.meta_test(inp,way=way,shot=shot,query_shot=query_shot)
-        
-        #------------------------观察距离------------------------------#
-        dist=model.out_neg_l2_dist(inp,way=way,shot=shot,query_shot=query_shot)
-        
-        pdb.set_trace()
-        # inp [105,3,84,84]
-        # max_index [80]
-        # dist [80,5]
-        # target[80]
-        
-        list_dist=torch.cat((list_dist,dist),0)
-        list_max_index.append(max_index)
-        
-        
-        #------------------------end------------------------------#
 
         acc = 100*torch.sum(torch.eq(max_index,target)).item()/query_shot/way
         acc_list.append(acc)
@@ -59,9 +41,5 @@ def meta_test(data_path,model,way,shot,pre,transform_type,query_shot=16,trial=10
     if return_list:
         return np.array(acc_list)
     else:
-        mean,interval = get_score(acc_list) 
-        
-        # return mean,interval # 原本的返回值
-        #------------------------------------------#
-        return mean,interval,list_dist,list_max_index,target
-        #------------------------------------------#
+        mean,interval = get_score(acc_list)
+        return mean,interval
