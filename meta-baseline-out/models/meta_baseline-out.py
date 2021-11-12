22a3652f16a4e8691a68ac94b7ded53b49b8afb7
Jf-Chen <485133380@qq.com>
2021-11-12 09:12:48 +0800

    update
    修改train-meta部分

--- meta-baseline-out/models/meta_baseline-out.py
+++ meta-baseline-out/models/meta_baseline-out.py
@@ -5,12 +5,12 @@ import torch.nn.functional as F
 import models
 import utils
 from .models import register
+import pdb
 
+@register('meta-baseline-out')
+class MetaBaselineOut(nn.Module):
 
-@register('meta-baseline')
-class MetaBaseline(nn.Module):
-
-    def __init__(self, encoder, encoder_args={}, method='cos',
+    def __init__(self, encoder, encoder_args={}, method='cos-DN4',
                  temp=10., temp_learnable=True):
         super().__init__()
         self.encoder = models.make(encoder, **encoder_args)
@@ -32,7 +32,9 @@ class MetaBaseline(nn.Module):
         x_shot, x_query = x_tot[:len(x_shot)], x_tot[-len(x_query):]
         x_shot = x_shot.view(*shot_shape, -1)
         x_query = x_query.view(*query_shape, -1)
-
+        
+        pdb.set_trace()
+        
         if self.method == 'cos':
             x_shot = x_shot.mean(dim=-2)
             x_shot = F.normalize(x_shot, dim=-1)
@@ -41,6 +43,13 @@ class MetaBaseline(nn.Module):
         elif self.method == 'sqr':
             x_shot = x_shot.mean(dim=-2)
             metric = 'sqr'
+        elif self.method == 'cos-DN4':
+            # 特征逐个匹配，按照相似度排序
+            # 取所有特征作为loss,但要记录每个特征的相似度
+            x_shot = x_shot.mean(dim=-2)
+            x_shot = F.normalize(x_shot, dim=-1)
+            x_query = F.normalize(x_query, dim=-1)
+            metric = 'dot'
 
         logits = utils.compute_logits(
                 x_query, x_shot, metric=metric, temp=self.temp)
